9013c36159aaef428e3bbd641658c7d9fc42fe25
shicy <shicy85@163.com>
2020-08-07 15:18:06 +0800

    Request

---
+++ src/tool/Request.js
@@ -0,0 +1,17 @@
+/**
+ * Http请求自定义
+ * Created by shicy 2020-08-07
+ */
+
+export function get(url, params, callback) {
+
+}
+
+export function post(url, params, callback) {
+
+}
+
+///////////////////////////////////////////////////////////
+const function doRequest(method, url, params, callback) {
+
+};
